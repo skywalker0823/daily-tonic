3139bafba02a862f0a4d6f55e9f80fc58396001c
pikxl <skywalker0823@hotmail.com>
2024-05-13 21:23:40 +0800

    test build

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import base64
 import functions_framework
+import os, dotenv
+from fastapi import FastAPI
+
+dotenv.load_dotenv()
+
+TEST = os.getenv("TEST")
+
+def tester():
+    print(TEST)
 
 # Triggered from a message on a Cloud Pub/Sub topic.
 @functions_framework.cloud_event
@@ -7,3 +16,7 @@ def start_daily(cloud_event):
     # Print out the data from Pub/Sub, to prove that it worked
     print("OK")
     print(base64.b64decode(cloud_event.data["message"]["data"]))
+
+
+if __name__=="__main__":
+    tester()
