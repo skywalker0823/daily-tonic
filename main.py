4ae676655be453c8558691d2486ca352ecbea150
pikxl <skywalker0823@hotmail.com>
2024-05-14 18:29:05 +0800

    telegram test

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import base64
 import functions_framework
-import os, dotenv, datetime
+import os, dotenv, datetime, requests
 from fastapi import FastAPI
 
-dotenv.load_dotenv()
+def get_telegram_config():
+    dotenv.load_dotenv()
+    return {
+        "TELEGRAM_TOKEN": os.getenv("TELEGRAM_TOKEN_V1"),
+        "TELEGRAM_CHAT_ID": os.getenv("TELEGRAM_CHAT_ID_V1")
+    }
 
-TEST = os.getenv("TEST")
-
-def tester():
-    print(TEST+"2")
+def send_telegram_message(text, token, chat_id):
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    params = {"chat_id": chat_id, "text": text}
+    response = requests.get(url, params=params)
+    return response.json()
 
 # Triggered from a message on a Cloud Pub/Sub topic.
 @functions_framework.cloud_event
-def start_daily(cloud_event):
+def start_daily(cloud_event, text, token, chat_id):
     # Print out the data from Pub/Sub, to prove that it worked
     print("---"+str(datetime.datetime.now())+"---")
     print(base64.b64decode(cloud_event.data["message"]["data"]))
+    config = get_telegram_config()
+    response = send_telegram_message("Test message from function-daily-tonic at" + str(datetime.datetime.now()), config["TELEGRAM_TOKEN"], config["TELEGRAM_CHAT_ID"])
+    print(response)
+
+   
 
 
-# if __name__=="__main__":
-#     tester()
--- test.py
+++ test.py
@@ -1,4 +1,35 @@
-
 import datetime
+import dotenv
+import requests
+import os
+import logging
 
-print("---"+str(datetime.datetime.now())+"---")
+# 配置日志记录器
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# 获取 Telegram 配置
+def get_telegram_config():
+    dotenv.load_dotenv()
+    return {
+        "TELEGRAM_TOKEN": os.getenv("TELEGRAM_TOKEN"),
+        "TELEGRAM_CHAT_ID": os.getenv("TELEGRAM_CHAT_ID")
+    }
+
+# 发送消息到 Telegram
+def send_telegram_message(text, token, chat_id):
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    params = {"chat_id": chat_id, "text": text}
+    response = requests.get(url, params=params)
+    return response.json()
+
+# 测试函数
+def tester():
+    logger.info("---{}---".format(datetime.datetime.now()))
+    config = get_telegram_config()
+    logger.info("Telegram Config: {}".format(config))
+    response = send_telegram_message("Test message from function-daily-tonic", config["TELEGRAM_TOKEN"], config["TELEGRAM_CHAT_ID"])
+    logger.info("Telegram Response: {}".format(response))
+
+if __name__ == "__main__":
+    tester()
