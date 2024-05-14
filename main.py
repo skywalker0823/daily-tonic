5647234e5f337d626509208eec94fef95cac04f0
pikxl <skywalker0823@hotmail.com>
2024-05-14 19:31:58 +0800

    telegram test for nasa apod

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ import functions_framework
 # from fastapi import FastAPI
 
 
-def get_telegram_config():
+def get_configs():
     dotenv.load_dotenv()
     return {
         "TELEGRAM_TOKEN": os.getenv("TELEGRAM_TOKEN_V1", os.getenv("TELEGRAM_TOKEN")),
-        "TELEGRAM_CHAT_ID": os.getenv("TELEGRAM_CHAT_ID_V1", os.getenv("TELEGRAM_CHAT_ID"))
+        "TELEGRAM_CHAT_ID": os.getenv("TELEGRAM_CHAT_ID_V1", os.getenv("TELEGRAM_CHAT_ID")),
+        "NASA_KEY": os.getenv("NASA_KEY_V1", os.getenv("NASA_KEY"))
     }
 
 
@@ -25,19 +26,28 @@ def send_telegram_message(text, token, chat_id):
     return response.json()
 
 
-def get_nasa():
+def get_nasa(config):
     print("fetching nasa APOD...")
+    nasa_url = f'https://api.nasa.gov/planetary/apod?api_key={config["NASA_KEY"]}'
+    nasa_response = requests.get(nasa_url)
+    if nasa_response.status_code == 200:
+        nasa_data = nasa_response.json()
+        nasa_image = nasa_data["url"]
+        nasa_explanation = nasa_data["explanation"]
+        return {"nasa_image":nasa_image,"nasa_explanation":nasa_explanation}
+
 
 
 @functions_framework.cloud_event
 def start_daily(cloud_event):
     print("---" + str(datetime.datetime.now()) + "---")
     print(base64.b64decode(cloud_event.data["message"]["data"]))
-    config = get_telegram_config()
+    config = get_configs()
+    nasa_apod = get_nasa(config)
     messages = [
         {"text": "Message from function-daily-tonic at" + str(datetime.datetime.now())},
-        {"text": "https://digital-transformation.media/wp-content/uploads/2018/09/logo_gcp_vertical_rgb.png" + str(
-            datetime.datetime.now())},
+        {"text": nasa_apod["nasa_image"]},
+        {"text": nasa_apod["nasa_explanation"]}
     ]
     for message in messages:
         response = send_telegram_message(message["text"], config["TELEGRAM_TOKEN"], config["TELEGRAM_CHAT_ID"])
--- test.py
+++ test.py
@@ -9,27 +9,41 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # 获取 Telegram 配置
-def get_telegram_config():
+def get_configs():
     dotenv.load_dotenv()
     return {
-        "TELEGRAM_TOKEN": os.getenv("TELEGRAM_TOKEN"),
-        "TELEGRAM_CHAT_ID": os.getenv("TELEGRAM_CHAT_ID")
+        "TELEGRAM_TOKEN": os.getenv("TELEGRAM_TOKEN_V1", os.getenv("TELEGRAM_TOKEN")),
+        "TELEGRAM_CHAT_ID": os.getenv("TELEGRAM_CHAT_ID_V1", os.getenv("TELEGRAM_CHAT_ID")),
+        "NASA_KEY": os.getenv("NASA_KEY_V1", os.getenv("NASA_KEY"))
     }
 
-# 发送消息到 Telegram
-def send_telegram_message(text, token, chat_id):
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-    params = {"chat_id": chat_id, "text": text}
-    response = requests.get(url, params=params)
-    return response.json()
 
-# 测试函数
-def tester():
-    logger.info("---{}---".format(datetime.datetime.now()))
-    config = get_telegram_config()
-    logger.info("Telegram Config: {}".format(config))
-    response = send_telegram_message("Test message from function-daily-tonic", config["TELEGRAM_TOKEN"], config["TELEGRAM_CHAT_ID"])
-    logger.info("Telegram Response: {}".format(response))
+# 发送消息到 Telegram
+# def send_telegram_message(text, token, chat_id):
+#     url = f"https://api.telegram.org/bot{token}/sendMessage"
+#     params = {"chat_id": chat_id, "text": text}
+#     response = requests.get(url, params=params)
+#     return response.json()
+
+# # 测试函数
+# def tester():
+#     logger.info("---{}---".format(datetime.datetime.now()))
+#     config = get_configs()
+#     logger.info("Telegram Config: {}".format(config))
+#     response = send_telegram_message("Test message from function-daily-tonic", config["TELEGRAM_TOKEN"], config["TELEGRAM_CHAT_ID"])
+#     logger.info("Telegram Response: {}".format(response))
+
+def get_nasa():
+    config = get_configs()
+    nasa_url = f'https://api.nasa.gov/planetary/apod?api_key={config["NASA_KEY"]}'
+    nasa_response = requests.get(nasa_url)
+    if nasa_response.status_code == 200:
+        nasa_data = nasa_response.json()
+        nasa_image = nasa_data["url"]
+        nasa_explanation = nasa_data["explanation"]
+        print(nasa_image,nasa_explanation)
+
 
 if __name__ == "__main__":
-    tester()
+    # tester()
+    get_nasa()
